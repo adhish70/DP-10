1de8bb3c14b81ff720e045e6fd9be56711543f0c
Adhish Shrivastava <adhish70@gmail.com>
2022-01-13 10:07:39 -0500

    Initial Commit

---
+++ Problem-155 Burst Balloons.py
@@ -0,0 +1,11 @@
+# 312. Burst Balloons
+# https://leetcode.com/problems/burst-balloons/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        pass
---
+++ Problem-156 Super Egg Drop.py
@@ -0,0 +1,11 @@
+# 887. Super Egg Drop
+# https://leetcode.com/problems/super-egg-drop/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def superEggDrop(self, k: int, n: int) -> int:
+        pass
